d191423bd35eebcc56af2ec0aa2652a6cfc84a46
Mike Rousos <mikerou@microsoft.com>
2022-12-19 17:19:40 -0500

    Add serialization Id attributes to account state in Orleans sample

--- Orleans/Actors/AccountState.cs
+++ Orleans/Actors/AccountState.cs
@@ -5,8 +5,10 @@ namespace Account
     [GenerateSerializer]
     internal class AccountState
     {
+        [Id(0)]
         public double Balance { get; set; }
 
+        [Id(1)]
         public double InterestRate { get; set; }
     }
 }
